0fd39e428f9a24ea9f80bc3d3f58d7e91a7fa7fc
SimeonSimeonovIvanov <simeon_s._ivanov@abv.bg>
2018-05-12 18:15:23 +0300

    update

--- src/v0/svpwm.c
+++ src/v0/svpwm.c
@@ -144,7 +144,7 @@ void svpwmInitADC( void )
 	ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
 	ADC_InitStructure.ADC_ScanConvMode = ENABLE;
 	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
-	ADC_InitStructure.ADC_ExternalTrigConvEdge = 0;
+	ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
 	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
 	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
 	ADC_InitStructure.ADC_NbrOfConversion = 2;
